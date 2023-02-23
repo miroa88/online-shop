0ce8caa618ff1e1bf11205a9f5c46afb95c3bf47
Miro Abdalian <miroabdalian@gmail.com>
2023-02-23 15:15:19 -0800

    creating categories and scss styling

--- src/App.js
+++ src/App.js
@@ -1,10 +1,44 @@
 
+import './categories.style.scss';
 
 const App = () => {
-  return (
-    <div>
-      Online store
+  const categories = [
+    {
+      id: 1,
+      title: 'Rods'
+    },
+    {
+      id: 2,
+      title: 'Reels'
+    },
+    {
+      id: 3,
+      title: 'Accessories'
+    },
+    {
+      id: 4,
+      title: 'Dry Fly'
+    },
+    {
+      id: 5,
+      title: 'Wet Fly'
+    }   
+  ]
 
+  return (
+    <div className="categories-container">
+      {categories.map(category => (
+        <div key={category.id} className="category-container">
+          {/* <img /> */}
+          <div className="background-image-container"> 
+          </div>
+          <div className="category-body-container">
+            <h2>{category.title}</h2>
+            <p>Shop Now</p>
+          </div>
+        </div>
+        )
+       )}
     </div>
   );
 }
