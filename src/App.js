332b131f52483c8dab8519eb44b1cc99161c1d69
Miro Abdalian <miroabdalian@gmail.com>
2023-02-23 15:45:15 -0800

    adding urls

--- src/App.js
+++ src/App.js
@@ -5,34 +5,43 @@ const App = () => {
   const categories = [
     {
       id: 1,
-      title: 'Rods'
+      title: 'Rods',
+      imageUrl: "https://blueridgemountainlife.com/wp-content/uploads/2021/01/fly-fishing-474090_1920.jpg"
     },
     {
       id: 2,
-      title: 'Reels'
+      title: 'Reels',
+      imageUrl: "https://i0.wp.com/flylordsmag.com/wp-content/uploads/2021/06/DSC00022.jpg?fit=2048%2C1365&ssl=1"
     },
     {
       id: 3,
-      title: 'Accessories'
+      title: 'Accessories',
+      imageUrl: "https://i.ytimg.com/vi/2pGfuFbBSbk/maxresdefault.jpg"
     },
     {
       id: 4,
-      title: 'Dry Fly'
+      title: 'Dry Fly',
+      imageUrl: "https://www.flytyer.com/wp-content/uploads/2016/05/dry-fly-opener-web.jpg"
+
     },
     {
       id: 5,
-      title: 'Wet Fly'
+      title: 'Wet Fly',
+      imageUrl: "http://cdn.shopify.com/s/files/1/0031/2210/2339/articles/20-Top-Flies-for-Fly-Fishing-In-October.jpg?v=1604422380"
     }   
-  ]
+  ];
 
   return (
     <div className="categories-container">
       {categories.map(category => (
-        <div key={category.id} className="category-container">
-          {/* <img /> */}
-          <div className="background-image-container"> 
-          </div>
-          <div className="category-body-container">
+        <div key={category.id} className='category-container'>
+          <div
+            className='background-image'
+            style={{
+              backgroundImage: `url(${category.imageUrl})`,
+            }}
+          />
+          <div className='category-body-container'>
             <h2>{category.title}</h2>
             <p>Shop Now</p>
           </div>
