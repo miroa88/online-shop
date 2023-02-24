813dde01e994b407b6030fddbc842b172a5c5e8d
Miro Abdalian <miroabdalian@gmail.com>
2023-02-23 16:07:13 -0800

    adding directory and category components

--- src/App.js
+++ src/App.js
@@ -1,54 +1,8 @@
-
-import './categories.style.scss';
+import Directory from './components/directory/directory.component';
 
 const App = () => {
-  const categories = [
-    {
-      id: 1,
-      title: 'Rods',
-      imageUrl: "https://blueridgemountainlife.com/wp-content/uploads/2021/01/fly-fishing-474090_1920.jpg"
-    },
-    {
-      id: 2,
-      title: 'Reels',
-      imageUrl: "https://i0.wp.com/flylordsmag.com/wp-content/uploads/2021/06/DSC00022.jpg?fit=2048%2C1365&ssl=1"
-    },
-    {
-      id: 3,
-      title: 'Accessories',
-      imageUrl: "https://i.ytimg.com/vi/2pGfuFbBSbk/maxresdefault.jpg"
-    },
-    {
-      id: 4,
-      title: 'Dry Fly',
-      imageUrl: "https://www.flytyer.com/wp-content/uploads/2016/05/dry-fly-opener-web.jpg"
-
-    },
-    {
-      id: 5,
-      title: 'Wet Fly',
-      imageUrl: "http://cdn.shopify.com/s/files/1/0031/2210/2339/articles/20-Top-Flies-for-Fly-Fishing-In-October.jpg?v=1604422380"
-    }   
-  ];
-
   return (
-    <div className="categories-container">
-      {categories.map(category => (
-        <div key={category.id} className='category-container'>
-          <div
-            className='background-image'
-            style={{
-              backgroundImage: `url(${category.imageUrl})`,
-            }}
-          />
-          <div className='category-body-container'>
-            <h2>{category.title}</h2>
-            <p>Shop Now</p>
-          </div>
-        </div>
-        )
-       )}
-    </div>
+    <Directory />
   );
 }
 
---
+++ src/components/directory/directory.component.jsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import CategoryItem from '../category-item/category-item.component';
+
+import '../directory/directory.style.scss'
+
+const categories = [
+    {
+      id: 1,
+      title: 'Rods',
+      imageUrl: "https://blueridgemountainlife.com/wp-content/uploads/2021/01/fly-fishing-474090_1920.jpg"
+    },
+    {
+      id: 2,
+      title: 'Reels',
+      imageUrl: "https://i0.wp.com/flylordsmag.com/wp-content/uploads/2021/06/DSC00022.jpg?fit=2048%2C1365&ssl=1"
+    },
+    {
+      id: 3,
+      title: 'Accessories',
+      imageUrl: "https://i.ytimg.com/vi/2pGfuFbBSbk/maxresdefault.jpg"
+    },
+    {
+      id: 4,
+      title: 'Dry Fly',
+      imageUrl: "https://www.flytyer.com/wp-content/uploads/2016/05/dry-fly-opener-web.jpg"
+
+    },
+    {
+      id: 5,
+      title: 'Wet Fly',
+      imageUrl: "http://cdn.shopify.com/s/files/1/0031/2210/2339/articles/20-Top-Flies-for-Fly-Fishing-In-October.jpg?v=1604422380"
+    }   
+  ];
+
+const Directory = ({category}) => {
+  return (
+    <div className="categories-container">
+    {categories.map(category => (
+      <CategoryItem key={category.id} category={category}/>
+    ))}
+  </div>
+  )
+}
+
+export default Directory;
