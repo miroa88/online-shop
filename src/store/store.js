23ce8da0e724f9be044ecacadff78478eeeb2c26
Miro Abdalian <miroabdalian@gmail.com>
2023-03-28 11:53:44 -0700

    adding redux store

---
+++ src/store/store.js
@@ -0,0 +1,14 @@
+//combined place where all of the redux happened; where we receive actions and dispatch them into reducer to update store objects
+
+import { compose, createStore, applyMiddleware } from "redux";
+import logger from 'redux-logger';
+
+import { rootReducer } from './root-reducer';
+
+const middleWares = [process.env.NODE_ENV === 'development' && logger].filter(
+    Boolean
+  );
+
+const composedEnhancers = compose(applyMiddleware(...middleWares));
+
+export const store = createStore(rootReducer, undefined, composedEnhancers)
