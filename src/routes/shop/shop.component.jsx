8652b68e808e701048e0331f1b52b98d8d2328fb
Miro Abdalian <miroabdalian@gmail.com>
2023-03-28 11:55:39 -0700

    rapping shop component with categories provider

--- src/routes/shop/shop.component.jsx
+++ src/routes/shop/shop.component.jsx
@@ -1,14 +1,17 @@
 import { Route, Routes } from 'react-router-dom';
+import { CategoriesProvider } from '../../context/categories.context';
 
 import CategoriesPreview from '../categories-preview/categories-preview.component';
 import Category from '../category/category.component';
 
 const Shop = () => {
     return( 
-    <Routes>
-        <Route index element={<CategoriesPreview />}/>
-        <Route path=":category" element={<Category />} />
-    </Routes>
+    <CategoriesProvider>
+        <Routes>
+            <Route index element={<CategoriesPreview />}/>
+            <Route path=":category" element={<Category />} />
+        </Routes>
+    </CategoriesProvider>
     )   
 }
 export default Shop;
