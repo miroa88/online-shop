01d208e06d06e0fb6f30f8ecf5aa643b785d78ab
Miro Abdalian <miroabdalian@gmail.com>
2023-03-29 19:44:37 -0700

    updating app and index to implement redux

--- src/App.js
+++ src/App.js
@@ -1,32 +1,36 @@
 import { Routes, Route } from 'react-router-dom';
-// import { useDispatch } from 'react-redux';
-// import { useEffect } from "react";
-// import { 
-//     onAuthStateChangedListener,
-//     createUserDocumentFromAuth
-// } from './utils/firebase/firebase.utils';
-// import { setCurrentUser } from './store/user/user.action';
+import { useEffect } from "react";
 import Auth from './routes/authentication/authentication.component';
 import Navigation from './routes/navigation/navigation.component';
 import Home from './routes/home/home.component.jsx';
 import Shop from './routes/shop/shop.component';
 import CheckOut from './routes/checkout/checkout.component';
 
+import { 
+    onAuthStateChangedListener,
+    createUserDocumentFromAuth
+} from './utils/firebase/firebase.utils';
+
+import { setCurrentUser } from './store/user/user.action';
+
+//redux
+import { useDispatch } from 'react-redux';
+
 const App = () => {
-  // const dispatch = useDispatch();
+  const dispatch = useDispatch();
 
-  // useEffect(() => {
-  //   const unsubscribe = onAuthStateChangedListener((user) => {
-  //       if(user) {
-  //           // we should handle the case when user already exists to just return the reference
-  //            createUserDocumentFromAuth(user);
-  //       }
-  //       console.log("user", user)
-  //       dispatch(setCurrentUser(user));
-  //   });
-  //   return unsubscribe;
+  useEffect(() => {
+    const unsubscribe = onAuthStateChangedListener((user) => {
+        if(user) {
+            // we should handle the case when user already exists to just return the reference
+             createUserDocumentFromAuth(user);
+        }
+        // console.log("just user", user)
+        dispatch(setCurrentUser(user));
+    });
+    return unsubscribe;
 
-  // }, []);
+  }, [dispatch]); //dispatch added to dependencies array just for the error goes away
 
   return (
     <Routes>
--- src/index.js
+++ src/index.js
@@ -1,33 +1,36 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
-// import { Provider } from 'react-redux';
+
 
 import App from './App';
-// import { store } from './store/store'
 
-import { UserProvider } from './context/user.context';
+
+// import { UserProvider } from './context/user.context';
 //import { CategoriesProvider } from './context/categories.context';
-
 import { CartProvider } from './context/cart.context';
 
+
+//redux
+import { Provider } from 'react-redux';
+import { store } from './store/store';
 
 import './index.scss';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    {/* <Provider store={store}> */}
+    <Provider store={store}>
       <BrowserRouter>
           {/* <CategoriesProvider> */}
-          <UserProvider>
+          {/* <UserProvider> */}
             <CartProvider>
               <App />
             </CartProvider>
           {/* </CategoriesProvider> */}
-          </UserProvider>     
+          {/* </UserProvider>      */}
       </BrowserRouter>
-    {/* </Provider> */}
+    </Provider>
   </React.StrictMode>
 );
 
