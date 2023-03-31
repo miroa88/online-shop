72b6fc638114a34752cf9b3abb34e80b81db2420
Miro Abdalian <miroabdalian@gmail.com>
2023-03-30 18:55:09 -0700

    updating store.js

--- src/store/store.js
+++ src/store/store.js
@@ -1,11 +1,24 @@
 //combined place where all of the redux happened; where we receive actions and dispatch them into reducer to update store objects
 
 import { compose, createStore, applyMiddleware } from "redux";
-import logger from 'redux-logger';
+// import logger from 'redux-logger';
 
 import { rootReducer } from './root-reducer';
 
-const middleWares = [ logger];
+const loggerMiddleWare = (store) => (next) => (action) => {
+  if(!action.type) {
+    return next(action);
+  }
+  console.log('type: ', action.type);
+  console.log('payload: ', action.payload);
+  console.log('currentState: ', store.getState());
+
+  next(action);
+
+  console.log('nextState: ', store.getState());
+}
+
+const middleWares = [ loggerMiddleWare];
 
 const composedEnhancers = compose(applyMiddleware(...middleWares));
 
